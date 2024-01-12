b4449bb164c5eb7d52e4a51801180f22156639a6
frostyx-vk <vk.xam@yandex.ru>
2024-01-12 15:16:03 +0700

    add Notes component

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,9 @@
           <h1>{{ title }}</h1>
 
           <message v-if="message" :message="message"/>
-
           <newNote :note="note" @addNote="addNote" />
+          <notes :notes="notes" />
 
-          <div class="notes">
-            <div class="note" v-for="(note, index) in notes" :key="index">
-              <div class="note-header">
-                <p>{{ note.title }}</p>
-              </div>
-              <div class="note-body">
-                <p>{{ note.descr }}</p>
-                <span>{{ note.date }}</span>
-              </div>
-            </div>
-          </div>
         </div>
       </section>
     </div>
@@ -29,10 +18,12 @@
 <script>
 import message from '@/components/Message.vue'
 import newNote from '@/components/NewNote.vue'
+import notes from '@/components/Notes.vue'
 export default {
   components: {
     message,
     newNote,
+    notes,
   },
   data() {
     return {
---
+++ src/components/Notes.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="notes">
+        <div class="note" v-for="(note, index) in notes" :key="index">
+            <div class="note-header">
+                <p>{{ note.title }}</p>
+            </div>
+            <div class="note-body">
+                <p>{{ note.descr }}</p>
+                <span>{{ note.date }}</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        notes: {
+            type: Array,
+            required: true,
+        }
+    },
+}
+</script>
+
+<style lang="scss">
+.notes {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    padding: 40px 0;
+}
+.note {
+    width: 48%;
+    padding: 18px 20px;
+    margin-bottom: 20px;
+    background-color: #fff;
+    border-radius: 8px;
+}
+.note-header {
+    p {
+        font-size: 22px;
+        color: #402caf;
+    }
+}
+.note-body {
+    p {
+        margin: 10px 0;
+    }
+    span {
+        font-size: 14px;
+        color: #999;
+    }
+}
+</style>
